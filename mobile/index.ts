4f97267ec496af7eb3ac5095b3741625d315b711
김태우 <rlaxkd1226@naver.com>
2025-04-01 19:57:38 +0900

    feat: 로딩 페이지 & 네비게이션 구현 & 카카오톡 로그인 창

--- mobile/index.ts
+++ mobile/index.ts
@@ -1,7 +1,4 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
-import { LoginScreen } from './src/screens/LoginScreen';
-// import Homescreen from './src/screens/Homescreen';
 
-// 앱을 Expo 환경에서 등록
-registerRootComponent(LoginScreen);
+registerRootComponent(App);
